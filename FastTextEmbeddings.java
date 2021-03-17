f074f889a07b1604ed4fc8a0815c07f417b0639c
Alex Barbet <64604471+abarbet@users.noreply.github.com>
2021-03-17 11:24:26 -0400

    Create FastTextEmbeddings.java

---
+++ FastTextEmbeddings.java
@@ -0,0 +1,27 @@
+import org.apache.commons.lang.ArrayUtils;
+import org.deeplearning4j.models.fasttext.FastText;
+import java.io.File;
+
+public class FastTextEmbeddings {
+    final private File wiki_file = new File("./wiki.en.bin");
+    final private FastText wiki_model = new FastText(wiki_file);
+
+    public FastTextEmbeddings() {
+    }
+
+    public double[] getEmbeddings(String word1) {
+        double[] vec1 = this.wiki_model.getWordVector(word1);
+
+        return full_vec;
+    }
+
+    public static void main(String args[]) {
+        FastTextEmbeddings test = new FastTextEmbeddings();
+
+        double[] test_result = test.getEmbeddings("love");
+
+        for(int i=0; i<test_result.length;i++) {
+            System.out.println(test_result[i]);
+        }
+    }
+}
